3356398209e202f4104028ef50bf25d82f21fe01
rostun <rosa.m.tung@gmail.com>
2016-04-22 20:42:22 -0400

    stockPortfolio

---
+++ stockPortfolio/main.py
@@ -0,0 +1,15 @@
+import webapp2 # google's framework
+
+config = {'default-group': 'base-data'}
+
+app = webapp2.WSGIApplication([ # basic routes, take us to the three classes
+	('/nation', 'nation.Nation'), # country 
+	('/company', 'company.Company'), # company 
+], debug=True, config=config)
+
+# regular expressions
+app.router.add(webapp2.Route(r'/country/<id:[0-9]+> <:/?>', 'company.Company')) # will take a company and an id (string of integers), will look up an individual company
+app.router.add(webapp2.Route(r'/company/search', 'company.CompanySearch')) # searches a thing
+app.router.add(webapp2.Route(r'/nation', 'nation.Nation'))
+# how we add companies to countries
+app.router.add(webapp2.Route(r'/nation/<nid:[0-9]+>/company/<cid:[0-9]+><:/?>', 'nation.NationCompanies')) # countries have a list of companies 
---
+++ stockPortfolio/nation.py
@@ -0,0 +1,57 @@
+import webapp2
+from google.appengine.ext import ndb
+import db_models
+import json
+
+class Nation(webapp2.RequestHandler):
+	def post(self):
+		'''
+		Creates a Nation Entity
+		POST Body Variables:
+		nname - Required. Country Name
+		companies[] - Array of Company ids
+		'''
+		if 'application/json' not in self.request.accept:
+			self.response.status = 406
+			self.response.status_message = "Not Acceptable, API only supports application/json MIME type."
+			return
+		new_nation = db_models.Nation()
+		nname = self.request.get('nname', default_value=None)
+		companies = self.request.get_all('companies[]', default_value=None)
+		if ccname: 
+			new_nation.nname = nname
+		else
+			self.response.status = 400
+			self.response.status_message = "Invalid Request"
+		if companies:
+			for company in companies:
+				new_nation.companies.append(ndb.Key(db_models.Company, int(company)))
+		key = new_nation.put()
+		out = new_nation.to_dict()
+		self.response.write(json.dumps(out))
+		return
+
+#add company to a country
+class NationCompanies(webapp2.RequestHandler):
+	def put(self, **kwargs):
+		if 'application/json' not in self.request.accept:
+			self.response.status = 406
+			self.response.status_message = "Not Acceptable, API only supports application/json MIME type."
+			return
+		if 'nid' in kwargs:
+			nation = ndb.Key(db_models.Channel, int (kwargs['nid'])).get()
+			if not nation:
+				self.response.status = 404
+				self.response.status_message = "Nation not Found"
+				return
+		if 'cid' in kwargs:
+			company = ndb.Key(db_models.Company, int(kwargs['cid']))
+			if not company:
+				self.response.status = 404
+				self.response.status_message = "Company not Found"
+				return
+		if company not in nation.companies:
+			nation.companies.append(company)
+			nation.put()
+		self.response.write(json.dumps(nation.to_dict()))
+		return
